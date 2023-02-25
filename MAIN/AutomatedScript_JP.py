78ee2fc75787de30fc2598b0d271f8ed1fc79508
Judhi Prasetyo <judhi@judhi.net>
2023-02-25 15:38:24 +0400

    copied by JP

---
+++ MAIN/AutomatedScript_JP.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+import DetectedObjects_JP as DetectedObjects
+# from EpsonController import sendToEpson, DROP_POINT
+from getWorldCoordinates import getRealWorld
+
+home = "0 "
+
+reference_image = cv2.imread("MAIN/test/Objects3.png")
+resized_img = cv2.resize(reference_image, (0, 0), fx=0.6, fy=0.6)
+
+reference_image = resized_img
+# cropped_img2 = reference_image[80:1300, 140:1000]
+# cv2.imshow("frame", cropped_img2)
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+center_points = []
+
+src_points = np.float32([(412, 14), (1663, 14), (1663, 1059), (412, 1059)])
+dst_points = np.float32([(400, 10), (1623, 10), (1623, 1039), (400, 1039)])
+
+M = cv2.getPerspectiveTransform(src_points, dst_points)
+
+# output = cv2.warpPerspective(reference_image, M, (1700, 1200))
+
+
+detector = DetectedObjects.DetectedObjects(reference_image)
+detector.getCenterPoints()
+object_list = detector.getContours()
+sorted_list = detector.sortDetectedObjects(object_list)
+
+
+for item in sorted_list:
+    if item.center_point[0] < 1600 and item.center_point[1] < 900:
+        center_points.append(item.center_point)
+
+
+print("items detected: ", len(center_points))
+
+# for center in center_points:
+#     new_x, new_y = getRealWorld(center[0], center[1])
+#     print(f"{center} to real world: {new_x}, {new_y}")
+#     sendToEpson(x= new_x, y= new_y, robot_z=600, robot_u=0)
+#     sendToEpson()
+#     sendToEpson(-400, 500, 800, 0) # DROP POINT
+#     print(f"Destination Reached: {new_x}, {new_y}")
--- MAIN/findBasicShapes.py
+++ MAIN/findBasicShapes.py
@@ -2,15 +2,22 @@ import cv2
 
 # Load image
 image = cv2.imread("MAIN/test/Objects2.png")
+image = cv2.resize(image, (0, 0), fx=0.5, fy=0.5)
+
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 # Convert to grayscale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Blur the image to reduce noise
-blur = cv2.GaussianBlur(gray, (5, 5), 0)
+blur = cv2.GaussianBlur(gray, (1, 1), 0)
+cv2.imshow("blur",blur)
+cv2.waitKey(0)
 
 # Find circles
-circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 1, 5, param1=100, param2=30, minRadius=40, maxRadius=200)
+circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 1, 10, minRadius=5, maxRadius=200)
 
 # Draw circles on the image
 if circles is not None:
@@ -18,6 +25,9 @@ if circles is not None:
     for circle in circles:
         x, y, r = circle.astype(int)
         cv2.circle(image, (x, y), r, (0, 0, 255), 2)
+        print("Drawing")
+else:
+    print("No circles found")
 
 # Find rectangles
 contours, _ = cv2.findContours(blur, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -29,6 +39,6 @@ for contour in contours:
         cv2.drawContours(image, [approx], 0, (0, 255, 0), 2)
 
 # Show the image
-cv2.imshow("Image", image)
+cv2.imshow("Gray",image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
